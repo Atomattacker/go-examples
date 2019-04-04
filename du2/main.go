1de5a8b1aa42129acbe0d40fb9c2dff0cc1757ea
Atomattacker <goddessblessmeshm@163.com>
2019-04-04 17:12:04 +0800

    add new examples

--- du2/main.go
+++ du2/main.go
@@ -1,9 +1,20 @@
 package main
 
-import "flag"
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	"reflect"
+)
 
 var verbose = flag.Bool("v", false, "show verbose progress messages")
 
 func main() {
+	x := 123
+	t := reflect.TypeOf(x)
+	v := reflect.ValueOf(x)
+	fmt.Println(t.String())
+	fmt.Println(v.String())
 
+	http.ListenAndServe
 }
